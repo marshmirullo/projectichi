f5749455b01f766b991a28f9e649f2dd357086ea
amirulhakim <73978896+marshmirullo@users.noreply.github.com>
2021-06-13 01:15:01 +0800

    Update TheSociopath.java

--- src/thesociopath/TheSociopath.java
+++ src/thesociopath/TheSociopath.java
@@ -157,7 +157,7 @@ public class TheSociopath {
                             Friendship friendRelation = new Friendship();       //create new object of class Friendship
                             System.out.println("\n\n----------Event 6----------");
                             System.out.println("\n");
-                            students.printSpecificEdges(studentsID[0]);
+                            students.printEdges();
                             System.out.println("\nCongratulations for your graduations!\nNow let see how many friends you have made throughout the journey.");
                             System.out.println("\n---List of friends(including you)----");
                             System.out.println(Arrays.toString(studentsID));
